33d18004eec3b8f5b67ad25aeec8f51cbad0f0b7
James <adeshina413@gmail.com>
2022-09-26 00:31:49 +0100

    updated js file

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/hello',
- word = document.getElementById('inp-word'), 
- result = document.getElementById('result'), 
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/',
  sound = document.getElementById('sound'); 
 
+ let result = document.querySelector('.result');
+const btn = document.getElementById('search-btn');
+
+btn.addEventListener('click',()=>{
+    let inpWord = document.getElementById('inp-word').value;
+    fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then((data)=> {
+        console.log(data[0]);
+        result.innerHTML = `
+        <div class="word">
+            <h3>${inpWord}</h3>
+            <button>
+                <i class="fas fa-volume-up"></i>
+            </button>
+        </div>
+        <div class="details">
+            <p>${data[0].meanings[0].partOfSpeech}</p>
+            <p>${data[0].phonetics[1].text}</p>
+        </div>
+        <p class="word-meaning">
+            ${data[0].meanings[0].definitions[0].definition}
+        </p>
+        <p class="word-example">
+            ${data[0].meanings[0].definitions[0].example || ""}
+        </p>
+        `;
+    });
+
+});
